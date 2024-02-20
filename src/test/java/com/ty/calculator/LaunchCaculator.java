55da37179ac405643e3b38114528a903abee15b3
Subbu255 <subramani25597@gmail.com>
2024-02-20 09:11:58 +0530

    changes done

--- src/test/java/com/ty/calculator/LaunchCaculator.java
+++ src/test/java/com/ty/calculator/LaunchCaculator.java
@@ -1,6 +1,8 @@
 package com.ty.calculator;
 
+import java.io.File;
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,6 +10,8 @@ import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public class LaunchCaculator {
 	
@@ -15,9 +19,9 @@ public class LaunchCaculator {
 	@Test
 	public void lanchBrowser() throws Throwable {
 		
-		// File f = new File("C:\\Users\\Subhas bhosale\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js");
-		// AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(f).withIPAddress("127.0.0.1").usingPort(4723).withTimeout(Duration.ofSeconds(3000)).build();
-		// service.start();
+		 File f = new File("C:\\Users\\Subhas bhosale\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js");
+		 AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(f).withIPAddress("127.0.0.1").usingPort(4723).withTimeout(Duration.ofSeconds(3000)).build();
+		 service.start();
 
 		DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability("platformName", "Android");
